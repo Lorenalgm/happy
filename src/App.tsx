79ac5b416a3d0849952c99f0725199c824dd18c5
Lorenalgm <lorena@proesc.com>
2020-10-12 19:02:25 -0300

    Criado jsx e iniciada estilização da home

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/global.css';
+import './styles/pages/landing.css';
+import logoImg from './images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>Macapá</strong>
+          <span>Amapá</span>
+        </div>
+
+        <a href="" className="enter-app"> > </a>
+      </div>
     </div>
   );
 }
