98fff6f4c900f3c23ba5032f10f478a3a7339ab6
Lorenalgm <lorena@proesc.com>
2020-10-13 00:57:38 -0300

    Finalizada estilização da home

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
 import React from 'react';
 import './styles/global.css';
-import './styles/pages/landing.css';
-import logoImg from './images/logo.svg';
+import Routes from './routes';
 
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy" />
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças</p>
-        </main>
-
-        <div className="location">
-          <strong>Macapá</strong>
-          <span>Amapá</span>
-        </div>
-
-        <a href="" className="enter-app"> > </a>
-      </div>
-    </div>
+    <Routes />
   );
 }
 
